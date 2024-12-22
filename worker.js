771b20453d0fa3c7c5bf96ee2839c9b620a7f76c
Anton <51289553+fhspb@users.noreply.github.com>
2024-12-22 16:17:31 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     const urls = [
-      "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=8766a6c557282bb6ec2b7a3465be7b0d489d73a5874184f0928acc29f9658640",
-      "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=0711407d35ff943c161c001a2c27f64ed78aded0e8898fbd2f9013c468a3ed62"
+      `https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${env.TOKEN_TV1}`, // My TV
+      `https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=${env.TOKEN_TV2}`  // mr. K TV
     ];
 
     let responses = [];
@@ -26,7 +26,10 @@ export default {
     return new Response(JSON.stringify(responses, null, 2), {
       headers: { 'Content-Type': 'application/json' }
     });
-  }
+  },
+
+  async scheduled(event, env, ctx) {
+    console.log(`Scheduled event triggered at: ${event.scheduledTime}`);
+    // Ваши действия, которые должны выполняться по расписанию
+  },
 };
-
-
